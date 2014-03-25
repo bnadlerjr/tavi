43f387a1bf5545a3370f25b3491285a813730001
Mark Simpson <verdammelt@gmail.com>
2014-03-25 08:43:39 -0400

    Clarifying behavior when loading data with missing fields.
    If the database was written with an old schema the new model will
    contain new fields. These tests define how Tavi should act in that
    circumstance.

--- tavi/test/unit/document_test.py
+++ tavi/test/unit/document_test.py
@@ -247,6 +247,38 @@ class DocumentFindTest(unittest.TestCase):
     def test_count(self):
         self.assertEqual(3, self.Sample.count())
 
+class DocumentLoadOldSchemaTest(unittest.TestCase):
+    """Tests the loading of data without fields which now exist in model"""
+    class Sample(Document):
+        first_name = fields.StringField("first_name", required=True)
+        last_name = fields.StringField("last_name",  required=True)
+        email = fields.StringField("email", required=True, default="default email")
+        address = fields.StringField("address", required=False)
+
+    def setUp(self):
+        super(DocumentLoadOldSchemaTest, self).setUp()
+        client = MongoClient()
+        client.drop_database("test_database")
+        self.db = client['test_database']
+        self.ids = self.db.samples.insert(
+            [
+                {"first_name": "John", "last_name": "Doe"},
+                {"first_name": "Joe"},
+            ]
+        )
+
+    def test_new_non_required_field(self):
+        item = self.Sample.find_by_id(self.ids[0])
+        self.assertEqual(None, item.address)
+        self.assertEqual([], item.errors.full_messages)
+
+    def test_new_required_field(self):
+        item = self.Sample.find_by_id(self.ids[1])
+        self.assertEqual(['Last Name is required'], item.errors.full_messages)
+
+    def test_new_field_with_default(self):
+        item = self.Sample.find_by_id(self.ids[0])
+        self.assertEqual('default email', item.email)
 
 class EmbeddedDocumentTest(unittest.TestCase):
     class Sample(EmbeddedDocument):
