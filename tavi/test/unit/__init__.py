7bcad3cb83ece0f16cbf3cbec39878b8604d98f2
Bob Nadler <bnadlerjr@gmail.com>
2014-10-23 09:25:42 -0400

    Add LogCapture utility.

--- tavi/test/unit/__init__.py
+++ tavi/test/unit/__init__.py
@@ -1,4 +1,27 @@
 # -*- coding: utf-8 -*-
 from tavi import Connection
+import logging
 
 Connection.setup("test_database")
+
+
+class LogCapture(logging.Handler):
+    MSG_TYPES = ["debug", "info", "warning", "error", "critical"]
+
+    def __init__(self, *args, **kwargs):
+        self.reset()
+        logging.Handler.__init__(self, *args, **kwargs)
+
+    def __enter__(self):
+        logging.getLogger().addHandler(self)
+        return self
+
+    def __exit__(self, type_, value, traceback):
+        logging.getLogger().removeHandler(self)
+        self.close()
+
+    def emit(self, record):
+        self.messages[record.levelname.lower()].append(record.getMessage())
+
+    def reset(self):
+        self.messages = {t: [] for t in self.MSG_TYPES}
